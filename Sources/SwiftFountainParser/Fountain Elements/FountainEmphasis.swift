2334ca5d14cf766f1b814879ef24f3e22223babc
cartre12321 <carter.riddall@outlook.com>
2023-02-23 18:45:13 -0500

    Change FountainScript from struct to class

---
+++ Sources/SwiftFountainParser/Fountain Elements/FountainEmphasis.swift
@@ -0,0 +1,22 @@
+//
+//  FountainEmphasis.swift
+//  
+//
+//  Created by Carter Riddall on 2023-02-22.
+//
+
+import Foundation
+
+public class FountainEmphasis: FountainInlineElement {
+    
+    override var type: FountainElementToken {
+        .emphasis(text: self.content, style: self.style)
+    }
+    
+    var style: FountainEmphasisStyle
+    
+    init(content: String, style: FountainEmphasisStyle) {
+        self.style = style
+        super.init(content: content, allowsChildren: false)
+    }
+}
